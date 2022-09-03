2c5288917ffa20f06a00908a966298434334efde
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-09-03 22:19:48 +0000

    Added more comments

--- ch01/exercises/simple.py
+++ ch01/exercises/simple.py
@@ -13,11 +13,15 @@ floor_divide = 15 // 10
 floor_divide1 = -15 // 10
 #% is modulo (aka remainder) (e.g. 15 modulo 10 = 1.5 --> 5 is remainder)
 modulo = 15 % 10
-#% is modulo (aka remainder) (e.g. 10 modulo 15 = 10 
+#% is modulo (aka remainder) (e.g. 10 modulo 15 = 10)
 modulo1 = 10 % 15
+#% is modulo (aka remainder) (e.g. 10 modulo 10 = 0)
+#reason is there is no remainder
 modulo2 = 10 % 10
+#% is modulo (aka remainder) (e.g. 0 modulo 10 = 0)
+#reason is there is no remainder
 modulo3 = 0 % 10
-#this one outputs 0.6666666666.....
+#outputs 0.6666666666..... (or 2/3)
 divide1 = 10 / 15
 #printing the results
 print("10 * 5 =", multiply)
@@ -29,5 +33,5 @@ print("15 % 10 =", modulo)
 print("10 % 15 =", modulo1)
 print("10 % 10 =", modulo2)
 print("0 % 10 =", modulo3)
-#this gives a repeating decimal
+#this outputs a repeating decimal
 print("10 / 15 =", divide1)
