4ba56f8ebcccf57d7dc9c09190aaa8be94ccb4a7
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-09-07 22:47:16 +0000

    Exercise: My Turtle

---
+++ ch02/exercises/simple.py
@@ -0,0 +1,37 @@
+# import turtle
+# #from turtle import Turtle
+
+# my_turtle = turtle.Turtle() #Factories
+# #my_turtle = Turtle()
+
+# # my_turtle.forward(10)
+
+# # my_turtle.color("green")
+
+# # my_turtle.shape("turtle")
+
+# # my_turtle.left(90)
+# # my_turtle.forward(20)
+# # my_turtle.up() #lifts pen up
+# # my_turtle.forward(20)
+# # my_turtle.down()
+
+# # my_turtle.color("blue")
+# # my_turtle.forward(20)
+
+# # window = turtle.Screen()
+# # window.bgcolor("orange")
+# # window.exitonclick()
+
+# num_sides = int(input("Please enter the number of sides: "))
+# my_turtle.color("purple")
+# length = 50
+# angle = 360 / num_sides
+
+# for i in [angle] * num_sides:
+#   my_turtle.forward(length)
+#   my_turtle.left(i)
+
+# window = turtle.Screen()
+# window.bgcolor("white")
+# window.exitonclick()
