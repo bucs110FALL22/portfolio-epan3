74f6694fbd8fdb44aaa6d7be2bef33cd74ebc336
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-09-07 23:59:01 +0000

    Made change to ch03 lab

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,5 +1,7 @@
 import turtle #1. import modules
 import random
+import pygame
+import math
 
 #Part A
 window = turtle.Screen() # 2.  Create a screen
@@ -18,9 +20,42 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
+#generating random distance for turtles
+random_distance = random.randrange(1,100)
 
+#moving turtles forward
+michelangelo.forward(random_distance)
+leonardo.forward(random_distance)
+#resetting their position
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
+# for i in [angle] * num_sides:
+#   my_turtle.forward(length)
+#   my_turtle.left(i)
+#for loop
+new_distance = random.randrange(0,10)
+for i in [new_distance] * 10:
+  michelangelo.forward(i)
+  leonardo.forward(i)
+#not sure if its suppose to be this fast
+  
+#resetting their position
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 # PART B - complete part B here
+# pygame.init()
+# window = pygame.display.set_mode()
+# table = pygame.display.get_surface()
+
+# coords = [list]
+# num_sides = [int]
+# side_length = [int]
+# offset = [int]
+
+# rectangle1 = pygame.Rect(10, 30, 50, 70)
+
+# theta = (2.0 * math.pi * side_length) / num_sides
 
 
 window.exitonclick()
