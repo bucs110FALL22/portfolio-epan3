a4c6e04fa28211d8dbbb4212b0cb91d49980ddf0
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-09-12 21:17:27 +0000

    updated turtle myturtle.py

--- ch02/exercises/myturtle.py
+++ ch02/exercises/myturtle.py
@@ -3,9 +3,11 @@ import turtle
 
 my_turtle = turtle.Turtle()
 
+#list
+colors = ["red", "purple"]
+
 #creating turtle and color
 my_turtle.shape("turtle")
-my_turtle.color("purple")
 
 #variables
 length = 50
@@ -13,27 +15,21 @@ square = 4
 angle = 360 / square
 
 #for loop for square
-for i in [angle] * square:
-  my_turtle.forward(length)
-  my_turtle.left(i)
-
-#picks pen up from paper
-my_turtle.up()
-
-#moves the hand to the left 80 units
-my_turtle.backward(80)
-
-#puts the pen back down on paper
-my_turtle.down()
-
-#creates turtle and color
-my_turtle.color("red")
-
-#for loop for square
-for i in [angle] * square:
-  my_turtle.forward(length)
-  my_turtle.left(i)
-
+for c in colors:
+  my_turtle.color(c)
+  for i in [angle] * square:
+    my_turtle.forward(length)
+    my_turtle.left(i)
+  
+  #picks pen up from paper
+  my_turtle.up()
+  #moves the hand to the left 80 units
+  my_turtle.backward(80)
+  #puts the pen back down on paper
+  my_turtle.down()
+  
 #waits for the click before closing
 window = turtle.Screen()
 window.exitonclick()
+
+#note: constant can be a variable in all caps
