d6830428e8c495df15642153ecc92a40f2762478
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-09-12 21:57:50 +0000

    Finished Chapter 2: "Exercise: Drawing with Turtle"

---
+++ ch02/exercises/drawing.py
@@ -0,0 +1,28 @@
+import turtle
+
+#asking user for input on sides and length
+sides = input("Enter the number of sides: ")
+sides = int(sides)
+
+length = input("Enter the length of each side: ")
+length = int(length)
+
+#variables
+angle = 360 / sides
+angle = int(angle)
+
+#ask user to enter the color
+colors = input("Enter the color of shape: ")
+
+#creating turtle object
+turtle.shape("turtle")
+turtle.color(colors)
+
+#for loop for drawing desired shape
+for num in [angle] * sides:
+  turtle.forward(length)
+  turtle.left(num)
+
+#waits for the click before closing
+window = turtle.Screen()
+window.exitonclick()
