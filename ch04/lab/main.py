e57017f53776a2a379e06733212203285589876a
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-09-22 22:32:32 +0000

    Made 2nd dartboard

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -4,7 +4,7 @@ import pygame
 import math
 
 #setup for pygame and display
-screen_width = 228
+screen_width = 468
 screen_height = 225
 
 pygame.init()
@@ -23,30 +23,45 @@ red = (255,0,0)
 green = (0,255,0)
 yellow = (255, 255, 0)
 
-#circular dart coordinates below
+#blue player circular dart coordinates below
 center = (115,112)
 radius = 113.5
 width = 300
 xline = [(110,226),(110,110),(110,-1)]
-yline = [(0,110),(200,110),(400,110)]
+yline = [(0,110),(200,110),(456,110)]
+
+#red player circulate dart coordinates below
+square = [(234.7,0),(466.9,0),(466, 229.4),(234.7, 229.4)]
+center1 = (350,112)
+radius1 = 113.5
+width1 = 300
+xline1 = [(350,226),(350,110),(350,-1)]
 
 #changing title
 pygame.display.set_caption("Dart Game")
 
 #Part A)
-#drawing dartboard
+#drawing blue player dartboard
 window.fill(blue)
 pygame.draw.circle(window, red, center, radius, width)
 pygame.draw.polygon(window, black, xline)
 pygame.draw.polygon(window, black, yline)
 pygame.display.update()
 
+#drawing red player backboard and dartboard
+pygame.draw.polygon(window, red, square)
+pygame.draw.circle(window, blue, center1, radius1, width1)
+pygame.draw.polygon(window, black, xline1)
+pygame.draw.polygon(window, black, yline)
+pygame.display.update()
+
 #Part B)
+#blue player dart for loop
 dartWidth = 0
 for i in range(10):
   #random dart (x,y) dart values
-  dartx = random.randrange(0,screen_width)
-  darty = random.randrange(0,screen_height)
+  dartx = random.randrange(0,228)
+  darty = random.randrange(0,225)
   #settings variables to draw circle
   center1 = (dartx,darty)
   radius1 = 2.5
@@ -61,13 +76,47 @@ for i in range(10):
   if is_in_circle:
     pygame.draw.circle(window, green, center1, radius1, dartWidth)
     pygame.display.update()
-    pygame.time.wait(1000)
+    pygame.time.wait(500)
   else:
     pygame.draw.circle(window, yellow, center1, radius1, 
     dartWidth)
     pygame.display.update()
-    pygame.time.wait(1000)
-  
+    pygame.time.wait(500)
+
+#red player dart for loop
+dartWidth = 0
+for i in range(10):
+  #random dart (x,y) dart values
+  dartx = random.randrange(234,466)
+  darty = random.randrange(0,229)
+  #settings variables to draw circle
+  center1 = (dartx,darty)
+  radius1 = 2.5
+  center1 = (dartx,darty)
+  #determining if lands in circle
+  xdistance = dartx - 350
+  ydistance = darty - 112
+  distance_from_center = math.hypot(xdistance, ydistance)
+  screen_size = radius#(screen_width * screen_height) / 2
+  is_in_circle = distance_from_center <= screen_size
+  not_in_circle = distance_from_center > screen_size
+  if is_in_circle:
+    pygame.draw.circle(window, green, center1, radius1, dartWidth)
+    pygame.display.update()
+    pygame.time.wait(500)
+  else:
+    pygame.draw.circle(window, yellow, center1, radius1, 
+    dartWidth)
+    pygame.display.update()
+    pygame.time.wait(500)
+
+    
+
+#Part C)
+
+
+
+
 #pygame looping code so it won't close found online
 #make sure to click on the 'X' to close the program
 running = True
