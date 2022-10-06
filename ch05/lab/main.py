90deb91d2348828b4cd641666a4976f857a1edfa
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-10-06 21:56:40 +0000

    Completed Chapter 5 Lab: 3n+1

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -16,7 +16,7 @@ while n != 1:
     n = (n * 3) + 1
     print(n)
 
-# printing results
+# print results
 print('\nn is equal to ', n)
 print('The program has stopped...')
 print('The value of count is', count)
@@ -30,13 +30,13 @@ print('\n---------------------------')
 upper_limit = 21 # changed upper limit to 21 since range(2,20) doesn't include 20
 iters = {} # an empty dictionary
 num = 0 # num is the new count variable for part B
-attempt = 0 # tracking each new value (ex: attempt 1, value 2, attempt 2, value 3, etc...)
+attempt = 0 # track each new value (ex: attempt 1, value 2, attempt 2, value 3, etc...)
 
 # for loop
 for start in range(2, upper_limit):
-  num = 0 # resets count to 0 again
+  num = 0 # reset count to 0 again
   attempt += 1
-  value = start # sets the new starting value each time (ex: 2, 3, 4 ... 18, 19, 20)
+  value = start # set the new starting value each time (ex: 2, 3, 4 ... 18, 19, 20)
   print("\nAttempt #{}:".format(attempt))
   while start != 1:
     print("\nStart #: {}".format(start))
@@ -52,11 +52,11 @@ for start in range(2, upper_limit):
       print('Current: {}'.format(start))
   iters[value] = num
 
-# storing in iters in a new variable same as shown in instructions
-threeplus1_iters_dict=iters
+# store iters in a new variable as shown in instructions
+threeplus1_iters_dict = iters
 
-# printing entire dictionary for 3n + 1 for {value:num,value:num...value:num}
-print(threeplus1_iters_dict)
+# print entire dictionary for 3n + 1 for {value:num,value:num...value:num}
+print("\n",threeplus1_iters_dict)
 
 ### Part C
 # import pygame
@@ -65,47 +65,32 @@ import pygame
 # initialize pygame
 pygame.init()
 
-# creating display
+# create display
 display = pygame.display.set_mode()
 
-# settings colors
+# set colors
 white = [255,255,255]
 black = [0,0,0]
 green = [0,255,0]
 
-# vairables
+# variables
 upper_limit = 21
 iters = threeplus1_iters_dict
 max_so_far = 0
 max_val = 0
-scale = 5
+scale = 14
+new = {}
 
-# iters = zip(iters.values(), iters.keys())
-pairs = [(x, y) for (x, y) in iters.items()]
-print(pairs)
-
-# fill the screen with color
+# fill the screen with background color
 display.fill(black)
 
-# drawing graph
-pygame.draw.lines(display, green, False, pairs)
-new_display = pygame.transform.flip(display, False, True)
-new_display = pygame.transform.scale(new_display, (500,500))
-new_display.blit(new_display, (0, 0))
-pygame.display.update()
-
-# setting font
-font = pygame.font.Font(None, 20)
-msg = font.render("test", False, "blue")
-display.blit(msg, (10,10))
-pygame.display.update()
-
-# for loop
+# for loop for 3n+1 [2,20] (inclusive)
 for n in range(2, upper_limit):
   pygame.display.update()
-  num = 0 # resets count to 0 again
+  num = 0 # reset count to 0 again
   attempt += 1
-  value = n # sets the new starting value each time (ex: 2, 3, 4 ... 18, 19, 20)
+  value = n # set the new starting value each time (ex: 2, 3, 4 ... 18, 19, 20)
+  # loop stops at n = 1 and statements inside check for even or odd #s
   while n != 1:
     if (n % 2) == 0:
       num += 1
@@ -114,8 +99,38 @@ for n in range(2, upper_limit):
       num += 1
       n = (n * 3) + 1
   iters[value] = num
-print(iters)
+  # compare max_so_far with num, update max_val & max_so_far variables
+  if max_so_far < num:
+    max_val = value
+    max_so_far = num
+    print (f'Max Value: {max_val}\nMax So Far: {max_so_far}')
 
+  # new dictionary puts compared values above in it
+  new[max_val] = max_so_far
+  # printing new dictionary
+  print(new)
+
+  # convert dictionary coordinates to listed tuples
+  pairs = [(x * scale, y * scale) for (x, y) in iters.items()]
+  
+  # drawing graph
+  new_display = pygame.Surface(display.get_size())
+  pygame.draw.lines(new_display, green, False, pairs)
+  new_display = pygame.transform.flip(new_display, False, True)
+  display.blit(new_display, (0, 0))
+  pygame.display.update()
+
+  # set string with on screen text message
+  max_value_string = f'Max Value: {max_val}   Max So Far: {max_so_far}'
+
+  # render string variable to display on screen
+  font = pygame.font.Font(None, 20)
+  msg = font.render(max_value_string, False, white)
+  display.blit(msg, (0,0))
+  pygame.display.update()
+  pygame.time.wait(1000)
+
+# program runs continuously; closes when user presses 'X'
 running = True
 while running:
   for event in pygame.event.get():
