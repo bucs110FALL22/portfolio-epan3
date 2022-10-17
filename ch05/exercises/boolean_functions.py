a2e4ae00101fc5b1755321b91497d7bc97501598
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-10-17 22:51:27 +0000

    Updated Ver. of Boolean Functions

--- ch05/exercises/boolean_functions.py
+++ ch05/exercises/boolean_functions.py
@@ -1,39 +1,36 @@
 def percentage_to_letter(score=0):
   score = float(score)
   if score < 60:
-    print("F")
+    return "F"
   elif score < 70:
-    print("D")
+    return "D"
   elif score < 80:
-    print("C")
+    return "C"
   elif score < 90:
-    print("B")
+    return "B"
   elif score >= 90:
-    print("A")
+    return "A"
   return score
 inputscore = float(input("Enter your exam percentage: "))
-percentage_to_letter(inputscore)
+result = percentage_to_letter(inputscore)
+print(result)
 
 def is_passing(letter = None):
   
   if letter == "A":
-    print("True")
     return True
   elif letter == "B":
-    print("True")
     return True
   elif letter == "C":
-    print("True")
     return True
   elif letter == "D":
-    print("False")
     return False
   elif letter == "F":
-    print("False")
     return False
 
 inputletter = input("Enter your single letter grade: ")
-is_passing(inputletter)
+result = is_passing(inputletter)
+print(result)
 
   
 
