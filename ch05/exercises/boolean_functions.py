455df53b0d4783db8d92ec6a88854e5beb5546a4
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-10-19 20:55:10 +0000

    added solutions to chapter 5 exercise boolean functions

--- ch05/exercises/boolean_functions.py
+++ ch05/exercises/boolean_functions.py
@@ -30,3 +30,54 @@ print(result)
 
 result = is_passing(result)
 print(result)
+
+## Test
+# print(is_passing(percentage_to_letter(85)))
+# print(is_passing(percentage_to_letter(65)))
+# print(is_passing(percentage_to_letter(110)))
+# print(is_passing(percentage_to_letter(-1)))
+
+### Solution
+# def getGrade(percentage):
+#     letter = 'F'
+#     if percentage >= 90:
+#         letter = 'A'
+#     elif percentage >= 80:
+#         letter = 'B'
+#     elif percentage >= 70:
+#         letter = 'C'
+#     elif percentage >= 60:
+#         letter = 'D'
+#     return letter
+
+
+# def isPassing(letter):
+#     if letter == 'A': 
+#         return True
+#     elif letter == 'B':
+#         return True
+#     elif letter == 'C':
+#         return True
+#     return False
+
+# print(isPassing(getGrade(85)))
+# print(isPassing(getGrade(65)))
+# print(isPassing(getGrade(110)))
+# print(isPassing(getGrade(-1)))
+
+# def isPassing(letter):
+#     #method 1
+#     # if letter == "A" or letter == "B" or letter == "C":
+#     # if letter == 'A': 
+#     #     return True
+#     # elif letter == 'B':
+#     #     return True
+#     # elif letter == 'C':
+#     #     return True
+#     # return False
+#     # method 2
+#     # if letter in "ABC":
+#     #     return True
+#     # else:
+#     #     return False
+#     return letter in "ABC"
---
+++ ch05/exercises/scope.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Write the following functions:
+# Write a function that multiplies two numbers together using a loop and accumulator (no multiplication) and return the resulting value
+
+
+# Write a function that takes a number and exponent as parameters and raises the number to the exponent and return the resulting value (no exponentiation)
+
+
+# Write another function, called square, that takes a single parameter and squares it by only calling your multiplication function and return the resulting value
+# Test your code by calling each function with whatever value you choose, then printing their results, i.e. the return value.
