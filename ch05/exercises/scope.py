c363059bd9b849414f2691d5bbf8e99c42c6ef69
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-10-19 22:19:48 +0000

    Chapter 5 Exercise: Scope and Accumulation

--- ch05/exercises/scope.py
+++ ch05/exercises/scope.py
@@ -1,6 +1,35 @@
+#multiply
+def multiply(num1, num2):
+  result = 0
+  for i in range(num2):
+    result = result + num1
+  return result
 
+#exponent
+def exponent(num, exp):
+  result = 1
+  for i in range(exp):
+    result = result * num
+  return result
 
+def square(num):
+  return multiply(num, num)
 
+def main():
+  num1 = int(input("Enter 1st number to multiply: "))
+  num2 = int(input("Enter 2nd number to multiply: "))
+  multiplication = multiply(num1, num2)
+  print(multiplication)
+  
+  base = int(input("Enter base number: "))
+  power = int(input("Enter the power number: "))
+  exp = exponent(base, power)
+  print(exp)
+
+  base1 = int(input("Enter base number to square: "))
+  result = square(base1)
+  print(result)
+main()
 
 
 
@@ -21,3 +50,22 @@
 
 # Write another function, called square, that takes a single parameter and squares it by only calling your multiplication function and return the resulting value
 # Test your code by calling each function with whatever value you choose, then printing their results, i.e. the return value.
+
+
+
+# remove vowels code
+# def removeVowels(text):
+#   vowels = "aeiou"
+#   vowels += vowels.upper() #list of vowels
+#   newtext = "" #accumulator - startingn value is empty string
+#   for ch in text:
+#     if ch not in vowels:
+#       newtext += ch
+#   return newtext
+
+# def main():
+#   mystr = input("Enter a string: ")
+#   string = removeVowels(mystr)
+#   print(string)
+  
+# main()
