5bc00d5b68c4aeb771ea7a70342878affdd1a319
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-10-31 22:09:21 +0000

    shelter

---
+++ ch07/exercises/shelter.py
@@ -0,0 +1,9 @@
+import time
+
+class Shelter:
+  def __init__(self, arrival_date, adopt_date, name = None, Type = None):
+    self.name = name
+    self.arrival_date = time.strftime("%d/%m/%Y")
+    
+    
+    
