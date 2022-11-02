89d2978065fab2e202698e688a6578375a8dabc2
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-11-02 21:53:26 +0000

    Exercise: Rescue Shelter

--- ch07/exercises/shelter.py
+++ ch07/exercises/shelter.py
@@ -1,53 +1,27 @@
 import time
+import uuid
 
-# class Shelter:
-#   def __init__(self, arrival_date, adopt_date, name = None, Type = None):
-#     self.name = name
-#     self.arrival_date = time.strftime("%d/%m/%Y")
-
-    
 class Animal:
-  def __init__(self, name, type):
-    """
-    __init__ parameters
-    :param name: name of the animal
-    :param type: type of the animal
-    :return: none
-    """
-    self.name = name    # name of the animal
-    self.type = type    # type of the animal
-    self.id = id(self)  # id of the animal
-    self.date_of_adoption = time.strftime("%d/%m/%Y")   # date of adoption
-  
-  def setAdopted(self):
-    """
-    setAdopted parameters
-    :return: none
-    """
-    date = input("Enter the date of adoption(dd/mm/yy format): ")   # Take date of adoption from user
-    self.date_of_adoption = date                               # set the date of adoption
+    def __init__(self, name, type):
+        self.id = time.strftime("%d%m%M%S")
+        #self.id = self(id)
+        #self.id = uuid()
+        self.name = name
+        self.type = type
+        self.arrived = time.strftime("%d/%m/%Y")
+        self.adopted = None
 
-  def __str__(self):
-    """
-    __str__ parameters
-    :return: string
-    """
-    return "\nAnimal name: " + self.name + "\nAnimal type: " + self.type + \
-       "\nAnimal id: " + str(self.id) + \
-       "\nDate of adoption: " + self.date_of_adoption + ""    # return the string
+    def set_adopted(self):
+        self.adopted = time.strftime("%d/%m/%Y")
 
-
+    def __str__(self):
+        result_str = f"{self.name}[{self.type}]"
+        result_str += f"\narrived: {self.arrived}"
+        result_str += f"\nadopted:{self.adopted}"
+        return result_str
+        
 def main():
-  """
-  main function
-  :return: none
-  """
-  Robert = Animal("Robert", "dog")    # create an animal
-  print(Robert)                   # print the animal
-  #Setting new adoption date  
-  Robert.setAdopted()           # set the date of adoption
-  print(Robert)                # print the animal
-
-
-if __name__ == "__main__":
-  main()                       # call the main function
+      fido = Animal("fido", "cat")
+      fido.set_adopted()
+      print(fido)
+main()
