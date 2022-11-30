52541a3def1dd800e31195809c889243e663baa8
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-11-30 23:58:37 +0000

    webapi changes

--- ch09/exercises/webapi.py
+++ ch09/exercises/webapi.py
@@ -2,6 +2,6 @@ import json
 import requests
 
 def get_results():
-  response = requests.get("https://date.nager.at/api/v3/CountryInfo/143")
+  response = requests.get("https://date.nager.at/api/v3/CountryInfo/143").json()
   print(response)
 get_results()
