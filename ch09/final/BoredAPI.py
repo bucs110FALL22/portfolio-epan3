529b71578a5628f0ee42794809144121a67d2ded
Eric Pan <112440134+epan3@users.noreply.github.com>
2022-12-13 01:55:46 +0000

    Final Exam: API Usage

--- ch09/final/BoredAPI.py
+++ ch09/final/BoredAPI.py
@@ -2,33 +2,33 @@ import requests
 
 class BoredAPI:
   def __init__(self):
-    """
+    '''
     Initialize BoredAPI instance with instance variables api_url, participants, price, and payload.
-    """
+    '''
     self.api_url = 'https://www.boredapi.com/api/activity'
     self.participants = input('Enter # Of Partcipants Activity (must be > 0): ')
-    self.price = float(input('Enter the cost value for the Activity (must be bewteen 0 & 1 (0 being  free, excluding 1)): '))
+    self.price = float(input('Enter the cost value for the Activity (must be bewteen 0 & 1 (0 being free, excluding 1)): '))
     self.payload = {'participants': {self.participants}, 'price': {self.price}}
     
   def __str__(self):
-    """
+    '''
     Return the value of participants as a string.
-    """
+    '''
     return str(self.payload['participants'])
   
   def get(self):
-    """
+    '''
     Pull data from BoredAPI and return it in JSON format.
-    """
+    '''
     self.pull_data = requests.get(self.api_url, params=self.payload)
     self.data = self.pull_data.json()
     return self.data
 
   def format(self):
-    """
+    '''
     Check if the returned JSON data contains an activity, and if so, format it for readability.
     Otherwise, return a message indicating that the activity does not exist.
-    """
+    '''
     if self.data.get('activity'):
       self.formatted = "\nActivity: {activity}\nType: {type}\nPeople: {participants}\nPrice: {price}\nAccessibility: {accessibility}".format(**self.data)
       return self.formatted
--- ch09/final/allproxy.py
+++ ch09/final/allproxy.py
@@ -4,19 +4,15 @@ import NumbersAPI
 
 def Proxy():
   '''Sets a constant variable of 0'''
-  NUM = 0
-
   '''Creates instances of the BoredAPI and NumbersAPI classes'''
+  '''Gets API URLs from the BoredAPI and Numbers API classes and stores in results'''
+  '''Uses the format method to format the results and store in variables'''
+  '''Prints the formatted results from the API URLs'''
+  NUM = 0
   bored_api = BoredAPI.BoredAPI()
   numbers_api = NumbersAPI.NumbersAPI(NUM)
-
-  '''Gets API URLs from the BoredAPI and Numbers API classes and stores in results'''
   bored_results = bored_api.get()
   numbers_results = numbers_api.get()
-
-  '''Uses the format method to format the results and store in variables'''
   bored_formatted = bored_api.format()
   numbers_formatted = numbers_api.format()
-
-  '''Prints the formatted results from the API URLs'''
   print(f'{bored_formatted}\n{numbers_formatted}')
